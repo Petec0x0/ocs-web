8588d328d792afa04d8017d8050c0e4ba2400417
Petec0x0 <udehonyedikachi01@gmail.com>
2022-11-08 18:47:30 +0100

    File upload description

--- src/components/RegisterSection.js
+++ src/components/RegisterSection.js
@@ -40,7 +40,14 @@ const RegisterSection = () => {
 
    const handleSubmitReceiptUpload = (e) => {
       e.preventDefault();
-      if (!uploadedReceiptImg) return;
+      if (!uploadedReceiptImg){
+         Swal.fire({
+            title: 'Error!',
+            text: 'No file selected',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+         });
+      }
       setSubmittedReceipt(true);
       const reader = new FileReader();
       reader.readAsDataURL(uploadedReceiptImg);
@@ -1067,7 +1074,7 @@ const RegisterSection = () => {
                            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 fill-white stroke-teal-500" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                               <path strokeLinecap="round" strokeLinejoin="round" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                            </svg>
-                           <span className="text-gray-600 font-medium">Upload payment receipt</span>
+                           <span className="text-gray-600 font-medium">Select payment receipt</span>
                         </label>
                         <input onChange={handleReceiptUpload} id="upload" type="file" className="hidden" />
                         {
